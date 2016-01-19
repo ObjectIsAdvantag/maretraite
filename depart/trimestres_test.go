2e4e7552d299cacd3e656ee91b846dc8eeecc428
Steve Sfartz <steve.sfartz@gmail.com>
2016-01-19 18:26:30 +0100

    added tests for trimestres

---
+++ depart/trimestres_test.go
@@ -0,0 +1,27 @@
+// Copyright 2016, Stève Sfartz
+// Licensed under the MIT License
+
+package depart
+
+import (
+	"testing"
+)
+
+
+func TestTrimestresPourMoi(t *testing.T) {
+	input := "24/12/1971"
+	birthDate, err := parseDate(input)
+	if err != nil {
+		t.Errorf("parse error in periode check")
+	}
+
+	expected := 171
+	nombre, err := RechercherTrimestre(birthDate)
+	if err != nil {
+		t.Errorf("get error in trimestre check")
+	}
+	if nombre != expected {
+		t.Errorf("mauvais nombre de trimestres: %d au lieu de %d", nombre, expected)
+	}
+}
+
